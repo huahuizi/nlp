152e539424e79ada98f3e65febfaba65fd6c7652
fightingliu <fightingliu@tencent.com>
2016-11-22 14:23:00 +0800

    second commit

--- NLP/NewWordDiscover/src/grid/common/Tree.java
+++
@@ -1,9 +0,0 @@
-package grid.common;
-
-public class Tree<T> extends Node<T> {
-
-	public Tree(T value) {
-		super(value);
-	}
-
-}
--- NLP/NewWordDiscover/安装包/exe版/ReadMe.md
+++
@@ -1,5 +0,0 @@
-#  生成的文件在result.txt中
-
-# 程序启动前时会清空上次result.txt，请妥善备份
-
-#  common.dic不可缺少
